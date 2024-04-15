5bbc3c3d4fa93e84fefe56d4b66383f4f62f2387
victorgis <essangvictor@gmail.com>
2024-04-15 11:30:41 +0100

    initial upload

---
+++ client/src/App.vue
@@ -0,0 +1,227 @@
+<script setup>
+import { computed, onBeforeMount, ref } from "vue";
+import SrMessages from "./components/SrMessages.vue";
+
+// Reactive vars that we'll use in our template
+const readersList = ref(null);
+const readerId = ref(null);
+const reader = ref(null);
+const amount = ref(null);
+const paymentIntent = ref(null);
+
+// For error messages
+const messages = ref([]);
+
+// Get readers before mounting the component
+onBeforeMount(async () => {
+  const response = await fetch("/api/readers");
+  console.log("readers", response);
+  const result = await response.json();
+  readersList.value = result.readersList;
+});
+
+// Process payment click handler
+const processPayment = async () => {
+  const response = await fetch("/api/readers/process-payment", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ amount: amount.value, readerId: readerId.value }),
+  });
+  const result = await response.json();
+  const { error } = result;
+  if (error) {
+    addMessage(error.message);
+    return;
+  }
+  reader.value = result.reader;
+  paymentIntent.value = result.paymentIntent;
+  addMessage(
+    `processing payment for ${amount.value} on reader ${reader.value.label}`
+  );
+};
+
+// Cancel action click handler
+const cancelAction = async () => {
+  const response = await fetch("/api/readers/cancel-action", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ readerId: readerId.value }),
+  });
+  const result = await response.json();
+  const { error } = result;
+  if (error) {
+    addMessage(error.message);
+    return;
+  }
+  reader.value = result.reader;
+  // paymentIntent.value = result.paymentIntent;
+  addMessage(
+    `Canceled reader action on ${reader.value.label} on reader ${reader.value.id}`
+  );
+  reset();
+};
+
+// Simulate payment click handler
+const simulatePayment = async () => {
+  const response = await fetch("/api/readers/simulate-payment", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ readerId: reader.value.id }),
+  });
+  const result = await response.json();
+  const { error } = result;
+  if (error) {
+    addMessage(error.message);
+    return;
+  }
+  addMessage(
+    `Simulating a customer tapping their card on simulated reader ${reader.value.id} for payment`
+  );
+};
+
+// Capture payment click handler
+const capturePayment = async () => {
+  // const paymentIntentId = ''
+  const response = await fetch("/api/payments/capture", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ paymentIntentId: paymentIntent.value.id }),
+  });
+  const result = await response.json();
+  const { error } = result;
+  if (error) {
+    addMessage(error.message);
+    return;
+  }
+  paymentIntent.value = result.paymentIntent;
+  addMessage(
+    `Capture payment for ${paymentIntent.value.id} ${reader.value.id}`
+  );
+  reset();
+};
+
+// Helpers
+function reset() {
+  paymentIntent.value = null;
+  amount.value = null;
+  reader.value = null;
+}
+
+function addMessage(message) {
+  messages.value.push(message);
+}
+
+// Computed properties to handle enabling and disabling of buttons
+// Don't worry about this stuff: it'll depend on your client.
+const isSimulateable = computed(() => {
+  if (reader.value) {
+    return reader.value?.device_type?.includes("simulated") &&
+      paymentIntent?.value?.id
+      ? true
+      : false;
+  }
+  return false;
+});
+
+const isCapturable = computed(() => {
+  return paymentIntent?.value?.id ? true : false;
+});
+
+const isProcessable = computed(() => {
+  return amount.value >= 100 && readerId.value ? true : false;
+});
+</script>
+
+<template>
+  <div class="sr-root">
+    <main class="sr-main">
+      <h2>Collecting Payments with Stripe Terminal</h2>
+      <p>Select a reader and input an amount for the transaction.</p>
+      <section>
+        <div>
+          <p>
+            <strong>Payment Intent ID:</strong
+            ><span v-if="paymentIntent">{{ paymentIntent.id }}</span>
+          </p>
+          <p>
+            <strong>Payment Intent status:</strong
+            ><span v-if="paymentIntent">{{ paymentIntent.status }}</span>
+          </p>
+        </div>
+        <p>
+          <strong>Reader Status:</strong>
+          <span v-if="reader">{{ reader?.action?.status }}</span>
+        </p>
+      </section>
+      <form id="confirm-form">
+        <label>Select Reader: </label>
+        <select
+          v-model="readerId"
+          name="reader"
+          id="reader-select"
+          class="sr-select"
+        >
+          <option value="none" selected disabled>Select a reader</option>
+          <option v-for="r in readersList" :value="r.id" :key="r.id">
+            {{ r.label }} ({{ r.id }})
+          </option>
+        </select>
+        <section class="sr-form-row">
+          <label for="amount">Amount:</label>
+          <input v-model="amount" id="amount" class="sr-input" />
+        </section>
+        <section class="button-row">
+          <button
+            type="button"
+            id="capture-button"
+            @click="processPayment"
+            :disabled="!isProcessable"
+          >
+            Process
+          </button>
+          <button
+            type="button"
+            id="capture-button"
+            @click="capturePayment"
+            :disabled="!isCapturable"
+          >
+            Capture
+          </button>
+        </section>
+        <section class="button-row">
+          <button
+            id="simulate-payment-button"
+            @click="simulatePayment"
+            type="button"
+            :disabled="!isSimulateable"
+          >
+            Simulate Payment
+          </button>
+          <button @click="cancelAction" id="cancel-button" type="button">
+            Cancel
+          </button>
+        </section>
+        <sr-messages :messages="messages" />
+      </form>
+    </main>
+  </div>
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ server/server.js
@@ -0,0 +1,87 @@
+require("dotenv").config({ path: "./.env" });
+const express = require("express");
+const app = express();
+app.use(express.json({}));
+const cors = require("cors");
+app.use(express.static("../client/dist"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2020-08-27",
+  appInfo: {
+    // For sample support and debugging, not required for production:
+    name: "stripe-samples/terminal-series/stripe-terminal-collect-payments",
+    version: "0.0.1",
+    url: "https://github.com/stripe-samples",
+  },
+});
+
+app.get("/api/readers", async (req, res) => {
+  try {
+    const { data: readers } = await stripe.terminal.readers.list();
+    res.json({ readersList: readers });
+  } catch (e) {
+    res.send({ error: { message: e.message } });
+  }
+});
+
+app.post("/api/readers/process-payment", async (req, res) => {
+  try {
+    const { amount, readerId } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "usd",
+      amount,
+      payment_method_types: ["card_present"],
+      capture_method: "manual",
+    });
+
+    const reader = await stripe.terminal.readers.processPaymentIntent(
+      readerId,
+      {
+        payment_intent: paymentIntent.id,
+      }
+    );
+    res.send({ reader, paymentIntent });
+  } catch (error) {
+    res.send({ error: { message: error.message } });
+  }
+});
+
+app.post("/api/readers/simulate-payment", async (req, res) => {
+  try {
+    const { readerId } = req.body;
+    const reader =
+      await stripe.testHelpers.terminal.readers.presentPaymentMethod(readerId);
+    res.send({ reader });
+  } catch (error) {
+    res.send({ error: { message: e.message } });
+  }
+});
+
+app.post("/api/payments/capture", async (req, res) => {
+  try {
+    const { paymentIntentId } = req.body;
+    const paymentIntent = await stripe.paymentIntents.capture(paymentIntentId);
+    res.send({ paymentIntent });
+  } catch (error) {
+    res.send({ error: { message: e.message } });
+  }
+});
+
+app.post("/api/readers/cancel-action", async (req, res) => {
+  try {
+    const { readerId } = req.body;
+    const reader = await stripe.terminal.readers.cancelAction(readerId);
+    res.send({ reader });
+  } catch (error) {
+    res.send({ error: { message: e.message } });
+  }
+});
+
+app.listen(4242, () =>
+  console.log(`Node server listening at http://localhost:4242`)
+);
